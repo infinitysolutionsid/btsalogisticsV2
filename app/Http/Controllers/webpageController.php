b0de999b4e8846129012116e0fcddd2bef8dd5fc
bintangjtobing <hello@bintangtobing.com>
2022-03-14 11:47:44 +0700

    crypt fix

--- app/Http/Controllers/webpageController.php
+++ app/Http/Controllers/webpageController.php
@@ -14,6 +14,7 @@ use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Crypt;
 
 class webpageController extends Controller
 {
